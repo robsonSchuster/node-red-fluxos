66d5a33c827752b680fee00addfc7083204c311c
robsonSchuster <68903975+robsonSchuster@users.noreply.github.com>
2025-02-13 15:11:33 -0300

    Update settings.js

--- node-red-flows/settings.js
+++ node-red-flows/settings.js
@@ -1,5 +1,5 @@
 module.exports = {
     flowFile: process.env.FLOWS_FILE_PATH || "/opt/render/node-red-flows/flows.json",
     credentialSecret: "minhaSenhaSegura",
-    uiPort: process.env.PORT || 1880
+    uiPort: process.env.PORT || 1880,
 };
