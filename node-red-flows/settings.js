7264c31d6c86aade89101c35d90a77114a1c5543
robsonSchuster <68903975+robsonSchuster@users.noreply.github.com>
2025-02-13 16:09:14 -0300

    Update settings.js

--- node-red-flows/settings.js
+++ node-red-flows/settings.js
@@ -1,7 +1,7 @@
 module.exports = {
     // Define o caminho do arquivo de fluxos
-    flowsFile: '/opt/render/node-red-flows/flows.json',
-    
+    //flowsFile: '/opt/render/node-red-flows/flows.json',
+    flowsFile: '/opt/render/project/src/node-red-fluxos/flows.json',
     // Define uma senha segura para criptografar credenciais
     credentialSecret: "minhaSenhaSegura",
 
