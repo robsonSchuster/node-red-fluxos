b6775b695515b2d6fe2097ddb48d07b725d051f5
robsonSchuster <68903975+robsonSchuster@users.noreply.github.com>
2025-02-13 18:38:04 -0300

    Update settings.js

--- node-red-flows/settings.js
+++ node-red-flows/settings.js
@@ -1,16 +1,11 @@
 module.exports = {
   flowFile: '/opt/render/project/src/data/flows.json', // Caminho para o arquivo de flows
   credentialSecret: "sua-chave-secreta-aqui", // Chave secreta para criptografar credenciais
-
-  // Configurações de autenticação (opcional)
-  adminAuth: null,
-
-  // Configurações de UI (opcional)
+  userDir: '/opt/render/project/src/data', // Diretório de usuário personalizado
+  adminAuth: null, // Autenticação de administrador (opcional)
   ui: {
     path: "/ui", // Caminho da interface do usuário
   },
-
-  // Configurações de logging
   logging: {
     console: {
       level: "info", // Nível de log
@@ -18,22 +13,16 @@ module.exports = {
       audit: false,
     },
   },
-
-  // Configurações de contexto
   contextStorage: {
     default: {
       module: "memory", // Armazenamento de contexto em memória
     },
   },
-
-  // Configurações de editor
   editorTheme: {
     projects: {
       enabled: false, // Desabilita projetos (opcional)
     },
   },
-
-  // Configurações de segurança
   security: {
     apiMaxLength: "10mb", // Tamanho máximo de payload para APIs
   },
