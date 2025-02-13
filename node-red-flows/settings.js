a130c7510e477b557d625fe23695f6c6916000e3
robsonSchuster <68903975+robsonSchuster@users.noreply.github.com>
2025-02-13 16:46:16 -0300

    Update settings.js

--- node-red-flows/settings.js
+++ node-red-flows/settings.js
@@ -1,6 +1,9 @@
 module.exports = {
-    flowsFile: process.env.FLOWS_FILE || '/opt/render/node-red-flows/flows.json',
-    credentialSecret: process.env.CREDENTIAL_SECRET || "minhaSenhaSegura",
-    uiPort: process.env.PORT || 1880,
-    userDir: '/opt/render/node-red-flows',
+    flowsFile: '/opt/render/node-red-flows/flows.json', // caminho do arquivo de fluxos
+    credentialSecret: "minhaSenhaSegura", // senha de criptografia
+    uiPort: process.env.PORT || 1880, // porta do servidor
+    userDir: '/opt/render/node-red-flows', // diretório de usuário do Node-RED
+    editorTheme: {
+        projects: { enabled: false } // desabilita projetos
+    }
 };
